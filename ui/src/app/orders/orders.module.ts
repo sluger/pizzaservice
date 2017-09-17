022c0b6cfd743dd26a9c6fab9e89d22a1b807943
Stefan Luger <lug.stefan@gmail.com>
2017-09-17 21:43:00 +0200

    orders admin view - not pretty

--- ui/src/app/orders/orders.module.ts
+++ ui/src/app/orders/orders.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { OrdersService } from './orders.service';
+import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    PipesModule
   ],
   declarations: [
     OrdersComponent,
