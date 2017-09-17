86b4ff953baf86c37c938c65bc3e2fa823aa84f7
Stefan Luger <lug.stefan@gmail.com>
2017-09-17 12:50:11 +0200

    code cleanup

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { PizzasModule } from './pizzas/pizzas.module';
 
